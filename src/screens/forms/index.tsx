e0b202e3516295a7205223500b33d82bf94d465d
Anny Confessor <anny.confessor@gmail.com>
2022-09-26 17:25:43 -0300

    fix: prop's forms to modal

--- src/screens/forms/index.tsx
+++ src/screens/forms/index.tsx
@@ -7,35 +7,94 @@ import "./styles.css"
 
 import { Container, Wrapper, Box, WrapperButtons, TextWrapper } from "./styles.tsx";
 
-import { mockDataInfos } from "./mocks.ts";
-import useLocalstorage from "../../hooks/useLocalstorage.tsx";
 
 const Forms = () => {
-const [modal, setModal] = useState(false);
-const [text, setText] = useLocalstorage("name", "initialValue")
+  const [name, setName] = useState("name")
+  const [lastName, setLastName] = useState("lastName")
+  const [email, setEmail] = useState("email")
+  const [location, setLocation] = useState("location")
+  const [note, setNote] = useState("note")
 
-  const handleModal = () => {
-    setModal(true);
+  const handleName = (e) => {
+    e.preventDefault()
+
+    setName(e.target.value)
+  }
+
+  const handleLastName = (e) => {
+    e.preventDefault()
+
+    setLastName(e.target.value)
+  }
+
+  const handleEmail = (e) => {
+    e.preventDefault()
+
+    setEmail(e.target.value)
+  }
+
+  const handleLocation = (e) => {
+    e.preventDefault()
+
+    setLocation(e.target.value)
+  }
+
+  const handleNote = (e) => {
+    e.preventDefault()
+
+    setNote(e.target.value)
   }
 
   return (
     <>
     <Container>
       <Box>
-          <div><h2>React-forms</h2></div>
-          {mockDataInfos.map((item) => (
-            <>
-              <Wrapper key={item.id}>
-                <TextWrapper>{item.field}</TextWrapper>
-                  <Input 
-                    key={item.id}
-                    type="text"
-                    name="textInput"
-                    onchange={(e) => setText(e.target.value)}
-                  />
-              </Wrapper>
-            </>
-          ))}
+        <div><h2>React-forms</h2></div>
+        <Wrapper>
+          <TextWrapper>Name</TextWrapper>
+          <Input
+            type="text"
+            name="textInput"
+            onchange={handleName}
+          />
+        </Wrapper>
+
+        <Wrapper>
+          <TextWrapper>Sobrenome</TextWrapper>
+          <Input
+            type="text"
+            name="textInput"
+            onchange={handleLastName}
+          />
+        </Wrapper>
+
+        <Wrapper>
+          <TextWrapper>E-mail</TextWrapper>
+          <Input
+            type="text"
+            name="textInput"
+            onchange={handleEmail}
+          />
+        </Wrapper>
+
+        <Wrapper>
+          <TextWrapper>Estado</TextWrapper>
+          <Input
+            type="text"
+            name="textInput"
+            onchange={handleLocation}
+          />
+        </Wrapper>
+
+        <Wrapper>
+          <TextWrapper>observação</TextWrapper>
+          <Input
+            type="text"
+            name="textInput"
+            onchange={handleNote}
+          />
+        </Wrapper>
+        
         <WrapperButtons>
           <Button variant="reset">Limpar</Button>
           {/* TODO: adicionar a variacao de link button no componente de button */}
@@ -43,7 +102,7 @@ const [text, setText] = useLocalstorage("name", "initialValue")
         </WrapperButtons>
       </Box>
       <div className="modal" id="abrirModal">
-        <Modal />
+        <Modal name={name} lastName={lastName} email={email} region={location} note={note}/>
       </div>
     </Container>
     </>
